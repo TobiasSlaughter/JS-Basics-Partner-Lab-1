e3ec081fbf0426a378ae7dbb1da7a4e1fdb76aee
michael-mcmasters <jmcmasters411@gmail.com>
2020-11-16 16:08:00 -0500

    Finished FizzBuzz

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+let firstNumber = prompt("Give me a number");
+let secondNumber = prompt("Give me a second number");
+let result = firstNumber / secondNumber;
+console.log(result);
 
-let firstNumber = prompt("Give me a number")
-
-let secondNumber = prompt("Give me a second number")
+if (result % 3 == 0 && result % 5 == 0) {
+  console.log("FizzBuzz");
+} else if (result % 3 == 0) {
+  console.log("Fizz");
+} else if (result % 5 == 0) {
+  console.log("Buzz");
+}
